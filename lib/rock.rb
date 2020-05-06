73833a7c51ed1f2e8f857d65bb30c001efa51da7
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-05-06 12:04:10 -0700

    debugging code file.

--- lib/rock.rb
+++ lib/rock.rb
@@ -1,11 +1,20 @@
 require ('pry')
 class RPS 
+  def initialize
+  @player1_score
+  @player2_score
+  end
 
-  def initialize 
+  def player1_score
     @player1_score = 0
+  end
+  def player2_score
     @player2_score = 0
+  end
+
+
     while true
-    puts "Player Score: #{@player1_score}, /tPlayer2 Score: #{@player2_score}"
+      puts "Player Score: #{@player1_score}, /tPlayer2 Score: #{@player2_score}"
       puts "Player one choose paper (p), rock (r), or scissors (s)"
       @player1_input = gets.chomp
       puts "Player two choose paper (p), rock (r), or scissors (s)"
@@ -18,43 +27,48 @@ class RPS
         @player1_score += 1
       elsif @player1_input == "r" && @player2_input == "r"
         puts "both players chose rock, no points are added to either score"
+        puts "rock does not beat rock, try again"
       elsif @player1_input == "r" && @player2_input == "p"
         puts "player 1 chose rock, player 2 chose paper"
+        puts "paper beats rock"
         puts "player 2 wins the round"
         @player2_score += 1
       elsif @player1_input == "s" && @player2_input == "r"
         puts "player 1 chose scissors, player 2 chose rock"
+        puts "rock beats scissors"
         puts "player 2 wins the round"
         @player2_score += 1
       elsif @player1_input == "s" && @player2_input == "p"
         puts "player one chose scissors, player 2 chose paper"
+        puts "scissors beats paper"
         puts "player one wins"
         @player1_score += 1
       elsif @player1_input == "s" && @player2_input == "s"
         puts "both players chose scissors, it's a tie"
+        puts "scissors does not beat scissors, try again"
       elsif @player1_input == "p" && @player2_input == "r"
         puts "player 1 chose paper, player 2 chose rock"
+        puts "paper beats rock"
         puts "player 1 wins"
         @player1_score += 1
       elsif @player1_input == "p" && @player2_input == "p"
-        
+        puts "both players chose paper, its a tie"
+        puts "paper does not beat paper, try again"
+      else @player1_input == "p" && @player2_input == "s"
+        puts "player1 chose paper, player 2 chose scissors"
+        puts "scissors beats paper"
+        puts "player 2 wins"
+        @player1_score += 1
       end
         
-      if @player1_score == 2
+      if @player1_score == 3
         puts "player one is the winner"
         break
-      elsif @player2_score == 2
+      elsif @player2_score == 3
         puts "player 2 is the winner"
         break
       end
-
     end
-  end
   
-  def player1_score
-    @player1_score
-  end
-  def player2_score
-    @player2_score
-    end
+ 
 end 
--- spec/rock_spec.rb
+++ spec/rock_spec.rb
@@ -4,18 +4,21 @@ require ('rock')
 describe('RPS#playerscore') do
   it("starts new game with player scores set to zero") do
     game = RPS.new()
-    expect(game.initialize.player2_score()).to(eq(0))
+    expect(game.player2_score()).to(eq(0))
   end
 end
 
-describe('RPS#player1_score') do
-  it("Rock beats Scissors and adds point to player one score") do
-    game = RPS.new()
-    expect(game.player1_score()).to(eq(1))
-  end
-end
 
-describe('RPS#paperwin') do
-it("Paper beats rock and adds points to player two score") do
+describe('RPS#player one win') do
+it("Player one score equal to 2, beat player two") do
   game = RPS.new()
-  expect(game.player2_score()).to(eq(1))
+  expect(game.player1_score()).to(eq(2))
+  end
+end
+
+describe('RPS#player two win') do
+it("Player two score equal to 2, beat player one") do
+  game = RPS.new()
+  expect(game.player2_score()).to(eq(2))
+  end
+end
